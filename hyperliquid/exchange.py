342a65bb7248fd067983e6677dac795bbe3f63ed
Ian Dennis Miller <iandennismiller@users.noreply.github.com>
2025-05-26 10:47:29 -0400

    Support grouping on bulk orders

--- hyperliquid/exchange.py
+++ hyperliquid/exchange.py
@@ -129,7 +129,7 @@ class Exchange(API):
             order["cloid"] = cloid
         return self.bulk_orders([order], builder)
 
-    def bulk_orders(self, order_requests: List[OrderRequest], builder: Optional[BuilderInfo] = None) -> Any:
+    def bulk_orders(self, order_requests: List[OrderRequest], builder: Optional[BuilderInfo] = None, grouping: Optional[str] = "na") -> Any:
         order_wires: List[OrderWire] = [
             order_request_to_order_wire(order, self.info.name_to_asset(order["coin"])) for order in order_requests
         ]
@@ -138,6 +138,7 @@ class Exchange(API):
         if builder:
             builder["b"] = builder["b"].lower()
         order_action = order_wires_to_order_action(order_wires, builder)
+        order_action["grouping"] = grouping
 
         signature = sign_l1_action(
             self.wallet,
